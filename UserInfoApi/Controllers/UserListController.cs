4435bcdb4a0d2ab9c61f4de1ed5e6806f8a8f606
Armen <Armen@DESKTOP-IRSKKA4>
2023-08-17 11:01:05 +0400

    Add project files.

---
+++ UserInfoApi/Controllers/UserListController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+//using NLog;
+using System.Data.Common;
+using UserInfoApi.UserList;
+
+namespace UserInfoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserListController : ControllerBase
+    {
+        //private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        [HttpGet]
+        public async Task GetUserList()
+        {
+            
+            try
+            {
+               
+                HttpClient httpClient = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://jsonplaceholder.typicode.com/todos");
+            request.Method = HttpMethod.Get;
+            
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+            var responseString = await response.Content.ReadAsStringAsync();
+            List<MyList> data = JsonConvert.DeserializeObject<List<MyList>>(responseString);
+                //logger.Info("--------------------data---------------------");
+                //logger.Info(data);
+                
+                using (var context = new UserListContext())
+                {
+                    data.ForEach(x =>
+                    {
+                        context.Add(new MyList{
+                          UserId = x.UserId,
+                          Title = x.Title,
+                          Completed = x.Completed
+                        });
+
+                    });
+                    context.SaveChanges();
+                }               
+            }
+            catch(DbException dbEx)
+            {
+                //logger.Error(dbEx.Message);
+            }
+            catch(Exception ex)
+            {
+                //logger.Error(ex.ToString());
+            }
+        }
+    }
+}
---
+++ UserInfoApi/UserList/MyList.cs
@@ -0,0 +1,17 @@
+﻿namespace UserInfoApi.UserList
+{
+    public partial class MyList
+    {
+       
+        public MyList()
+        {
+            
+        }
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string? Title { get; set; }
+        public string? Completed { get; set; }
+    }   
+
+   
+}
